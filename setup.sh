9fef54bf9f43a1b12371672b332efd674602eba5
Nathan Skone <nathan@skone.org>
2019-10-22 14:12:15 -0700

    first commit

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Finder settings
+# TODO: Fill this in
+
+# Dock settings
+# TODO: Fill this in
+# Remove most items from dock
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap homebrew/cask-versions
+brew update
+
+# Setup terminal
+brew cask install iterm2
+# TODO: Install fish and configure iterm2
+
+# BROWSERS
+brew cask install firefox google-chrome
+
+# TODO: Configure firefox settings?
+# TODO: Install ublock-origin?
+
+# Install Slack from app store?
+
+# Install sublime text 2
+# Setup sublime text 2 license?
+
+# Install Java and Maven
+
+# Install Docker
+
+# Configure Safari
+# Turn on favorites bar
