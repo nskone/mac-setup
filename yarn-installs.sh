059d7ee92bd2d9bd05f1c104c0e6d8477a68e1b3
Nathan Skone <nathan@skone.org>
2019-11-15 12:47:20 -0800

    Yarn installs is not actually needed

--- yarn-installs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -x "$(which yarn)" ] ; then
-  echo "Please install yarn first!"
-  exit 1
-fi
-
-# Nevermind, instead install parcel-bundler on a per project basis!
-# yarn global add parcel-bundler
